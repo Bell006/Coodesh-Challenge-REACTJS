096bb625a591d976f6f600281bdb18f7efd36cd9
Bell <bellamancio006@gmail.com>
2023-07-31 15:33:38 -0300

    Setting deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://coodeshchallenge.onrender.com"
+    baseURL: "https://coodeshchallenge-node-js.onrender.com"
 })
